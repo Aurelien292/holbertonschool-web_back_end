dac468f796e49f9086dcf5d9e9c7d4fb40632610
aurelien292 <9960@holbertonstudents.com>
2025-03-27 14:54:25 +0100

    npm run lint -- --fix

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,32 +1,27 @@
 export default class Currency {
-	constructor(code, name) {
-	  // Initialisation des variables privées
-	  this._code = code;
-	  this._name = name;
-	}
-  
-	// Getter pour l'attribut _code
-	get code() {
-	  return this._code;
-	}
-  
-	// Setter pour l'attribut _code
-	set code(value) {
-	  this._code = value;
-	}
-  
-	// Getter pour l'attribut _name
-	get name() {
-	  return this._name;
-	}
-  
-	// Setter pour l'attribut _name
-	set name(value) {
-	  this._name = value;
-	}
-  
-	// Méthode pour afficher la devise sous le format name (code)
-	displayFullCurrency() {
-	  return `${this._name} (${this._code})`;
-	}
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
   }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  // Méthode pour afficher la devise sous le format name (code)
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
