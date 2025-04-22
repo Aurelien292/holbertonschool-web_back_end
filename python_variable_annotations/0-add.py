1f349fcdaac2be18b96a13e4276efc82a66e6498
aurelien292 <9960@holbertonstudents.com>
2025-04-22 14:40:12 +0200

    change dossier variable_annotation dans back end

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""
+    Adds two floating point numbers together and returns the sum.
+    """
+
+
 def add(a: float, b: float) -> float:
     """
     Adds two floating point numbers together and returns the sum.
