ea9d277829de8cd911ccbf2f8b10aaa7501e7a26
aurelien292 <9960@holbertonstudents.com>
2025-04-29 14:35:01 +0200

    rectification exs

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -4,7 +4,8 @@
     async_generator() à l'aide d'une compréhension asynchrone et les
     retourne sous forme de liste.
     """
-
+from typing import List
+import asyncio
 
 async_generator = __import__('0-async_generator').async_generator
 """
@@ -14,7 +15,7 @@ async_generator = __import__('0-async_generator').async_generator
     """
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
     Cette coroutine récupère les 10 nombres générés par async_generator()
     à l'aide d'une compréhension asynchrone et les retourne
