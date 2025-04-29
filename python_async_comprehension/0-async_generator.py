45975ff0302640c5038d509438b9ae98b6e0df41
aurelien292 <9960@holbertonstudents.com>
2025-04-29 14:05:28 +0200

    rectification exs

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python3
-"""Module contains function async generator
-
-Imports:
-    asyncio: asyncio module
-    random: random module
 """
+    Cette coroutine génère une séquence de 10 nombres aléatoires compris
+    entre 0 et 10
+    """
 import asyncio
 import random
 from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
-    """Function yields certain value between 0 and 10
-
-    Yields:
-        _type_: Value between 0 and 10
     """
-    i = 0
-    while i < 10:
+    Cette coroutine génère une séquence de 10 nombres aléatoires
+    compris entre 0 et 10, un nombre toutes les secondes. Chaque nombre
+    est généré de manière asynchrone, ce qui permet de ne pas bloquer
+    l'exécution du programme principal.
+
+    Yield:
+        _type_ : Un nombre flottant aléatoire compris entre 0 et 10
+        à chaque itération.
+    """
+    for _ in range(10):
         await asyncio.sleep(1)
+        # Génère un nombre aléatoire entre 0 et 10
         yield random.uniform(0, 10)
-        i += 1
--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -4,7 +4,14 @@
     async_generator() à l'aide d'une compréhension asynchrone et les
     retourne sous forme de liste.
     """
+
+
 async_generator = __import__('0-async_generator').async_generator
+"""
+    Cette coroutine récupère les 10 nombres générés par
+    async_generator() à l'aide d'une compréhension asynchrone et les
+    retourne sous forme de liste.
+    """
 
 
 async def async_comprehension():
