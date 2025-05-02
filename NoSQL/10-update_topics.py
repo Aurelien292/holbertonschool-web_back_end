7bf6c15cc36786b63fd38897c2712842ab317826
aurelien292 <9960@holbertonstudents.com>
2025-05-02 09:36:26 +0200

    add exs NoSQL

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,9 @@
+"""change topics of a school """
+
+
+def update_topics(mongo_collection, name, topics):
+    """Change topics of a school document based on the name."""
+    return mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
