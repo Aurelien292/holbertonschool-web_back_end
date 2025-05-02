3486e0c1b5b98adaaf5c319ed2f314b7a6812159
aurelien292 <9960@holbertonstudents.com>
2025-05-02 10:15:54 +0200

    add exs NoSQL

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -10,11 +10,11 @@ def log_stats():
 
     print(f'{x.count_documents({})} logs')
     print(f'Methods:')
-    print(f'\tGET: {x.count_documents({"method": "GET"})}')
-    print(f'\tPOST: {x.count_documents({"method": "POST"})}')
-    print(f'\tPUT: {x.count_documents({"method": "PUT"})}')
-    print(f'\tPATCH: {x.count_documents({"method": "PATCH"})}')
-    print(f'\tDELETE: {x.count_documents({"method": "DELETE"})}')
+    print(f'\tmethod GET: {x.count_documents({"method": "GET"})}')
+    print(f'\tmethod POST: {x.count_documents({"method": "POST"})}')
+    print(f'\tmethod PUT: {x.count_documents({"method": "PUT"})}')
+    print(f'\tmethod PATCH: {x.count_documents({"method": "PATCH"})}')
+    print(f'\tmethod DELETE: {x.count_documents({"method": "DELETE"})}')
     print(f'{x.count_documents({"method": "GET", "path": "/status"})} '
           'status check')
 
