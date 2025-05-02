359c3f1b3c65d3b4433fefea3b71e601365b30e6
aurelien292 <9960@holbertonstudents.com>
2025-05-02 10:43:19 +0200

    add exs NoSQL

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -4,20 +4,22 @@ Provide stats about Nginx logs stored in MongoDB
 '''
 from pymongo import MongoClient
 
-
 def log_stats():
     x = MongoClient('mongodb://localhost:27017').logs.nginx
 
-    print(f'{x.count_documents({})} logs')
-    print('Methods:')
-    print(f'    method GET: {x.count_documents({"method": "GET"})}')
-    print(f'    method POST: {x.count_documents({"method": "POST"})}')
-    print(f'    method PUT: {x.count_documents({"method": "PUT"})}')
-    print(f'    method PATCH: {x.count_documents({"method": "PATCH"})}')
-    print(f'    method DELETE: {x.count_documents({"method": "DELETE"})}')
-    print(f'{x.count_documents({"method": "GET", "path": "/status"})} '
-          'status check')
 
+    print(f'{x.count_documents({})} logs')
+
+ 
+    print('Methods:')
+    print(f'\tmethod GET: {x.count_documents({"method": "GET"})}')
+    print(f'\tmethod POST: {x.count_documents({"method": "POST"})}')
+    print(f'\tmethod PUT: {x.count_documents({"method": "PUT"})}')
+    print(f'\tmethod PATCH: {x.count_documents({"method": "PATCH"})}')
+    print(f'\tmethod DELETE: {x.count_documents({"method": "DELETE"})}')
+
+
+    print(f'{x.count_documents({"method": "GET", "path": "/status"})} status check')
 
 if __name__ == "__main__":
     log_stats()
