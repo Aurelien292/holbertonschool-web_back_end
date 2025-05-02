d2ab8fa0903daa6bf7786451750c1973d5adca98
aurelien292 <9960@holbertonstudents.com>
2025-05-02 10:11:11 +0200

    add exs NoSQL

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
+'''
+Stats about nginx logs stored in MongoDB
+'''
 from pymongo import MongoClient
+
 
 def log_stats():
     x = MongoClient('mongodb://localhost:27017/').logs.nginx
-    
+
     print(f'Methods:')
     print(f'\tGET: {x.count_documents({"method": "GET"})}')
     print(f'\tPOST: {x.count_documents({"method": "POST"})}')
     print(f'\tPUT: {x.count_documents({"method": "PUT"})}')
     print(f'\tPATCH: {x.count_documents({"method": "PATCH"})}')
     print(f'\tDELETE: {x.count_documents({"method": "DELETE"})}')
-    print (f'{x.count_documents({"method": "GET", "path": "/status"})} status check')
-    
+    print(f'{x.count_documents({"method": "GET", "path": "/status"})} '
+          'status check')
+
+
 if __name__ == "__main__":
-	log_stats()
+    log_stats()
