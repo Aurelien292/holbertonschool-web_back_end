937d75509dc7ed8a24d1f8043d6a7d360acdcd91
aurelien292 <9960@holbertonstudents.com>
2025-05-20 15:23:36 +0200

    add 4-5 project Node

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,7 +1,6 @@
 const http = require('http');
 
-const app = http.createServer((req, res) => {
-  console.log('Request received:', req.method, req.url); // En plus
+const app = http.createServer((req, res) => {s
   res.writeHead('200', { 'Content-Type': 'text/plain' });
   res.write('Hello Holberton School!\n');
   res.end();
