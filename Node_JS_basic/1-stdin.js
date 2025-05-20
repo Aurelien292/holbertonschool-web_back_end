ece3e530ba5c993e6d0a208a864ce4e9aaac33fe
aurelien292 <9960@holbertonstudents.com>
2025-05-20 14:45:36 +0200

    add 1-2-3 project Node

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.setEncoding('utf8');
 process.stdin.on('readable', () => {
   const UserName = process.stdin.read();
-  if (UserName) process.stdout.write(`Your name is: ${UserName}\n`);
+  if (UserName) process.stdout.write(`Your name is: ${UserName}`);
 });
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
