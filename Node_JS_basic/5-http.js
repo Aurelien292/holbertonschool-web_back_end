3a9450f3a98116f113e60155817571cef192e2e1
aurelien292 <9960@holbertonstudents.com>
2025-05-20 15:47:47 +0200

    add 4-5 project Node

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,42 +1,36 @@
 const http = require('http');
-const fs = require('fs');
 const countStudents = require('./3-read_file_async');
 
+const msg = 'This is the list of our students\n';
 
 const app = http.createServer((req, res) => {
-	
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	
-	if (req.url === '/'){
-		res.end('Hello Holberton School!\n');
-	}
-	else if (req.url === '/students') {
-		
-		const database = process.argv[2];
-		if (!database) {
-			res.statusCode = 500;
-			res.end('Database is missing\n');
-			return;
-		}
-	res.write('This is the list of our students\n');
-	countStudents(database)
-	.then ((message)=> {
-		
-		res.end(message.join('\n'));
-	})
-	.catch ((err) => {
-		res.statusCode = 500;
-		res.end('Cannot load the database\n');
-});
-	}
-	else {
-		res.statuscode = 404;
-		res.end('Not found\n');
-	}
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+
+  if (req.url === '/') {
+    res.end('Hello Holberton School!\n');
+  } else if (req.url === '/students') {
+    const database = process.argv[2];
+    if (!database) {
+      res.statusCode = 500;
+      res.end('Database is missing\n');
+      return;
+    }
+    res.write('This is the list of our students\n');
+    countStudents(database)
+      .then((message) => {
+        res.end(message.join('\n'));
+      })
+      .catch((err) => {
+        res.end(`${msg}${err.message}`);
+      });
+  } else {
+    res.statuscode = 404;
+    res.end('Not found\n');
+  }
 });
 app.listen(1245, () => {
-	console.log('Server running on port 1245');
+  console.log('Server running on port 1245');
 });
 
 module.exports = app;
