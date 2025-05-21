16f7e5b4cb534315c1483f7d783ed0627983306a
aurelien292 <9960@holbertonstudents.com>
2025-05-21 10:33:47 +0200

    add ex 7 node.JS

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const counStStudents = require('./3-read_file_async');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (request, resonse) => {
+  resonse.set('Content-Type', 'text/plain');
+  resonse.send('Hello Holberton School!');
+});
+
+app.get('/students', async (request, response) => {
+  const path = process.argv[2];
+  const header = 'This is the list of our students\n';
+
+  try {
+    const result = await counStStudents(path);
+    response.set('Content-Type', 'text/plain');
+    response.send(`${header}${result}\n`);
+  } catch (error) {
+    response.set('Content-Type', 'text/plain');
+    response.send(`${header}Cannot load the database\n`);
+  }
+});
+app.listen(port);
+module.exports = app;
